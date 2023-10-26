b395f66cdbdba042cc0b1c24663d6d0639d23435
ardududududuinoman <shalompmc0505@naver.com>
2023-10-26 22:03:06 +0900

    build bugfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,22 +13,6 @@ export default function Home(): JSX.Element {
     <main className="min-w-full flex flex-col items-center justify-center p-24">
       <GuildInfoArea />
       <WorldBuff />
-<<<<<<< HEAD
-      <a
-        className="text-violet-800 underline"
-        href="https://cafe.naver.com/steamindiegame/13454124"
-      >
-        임시공지
-      </a>
-      <a
-        className="text-violet-800 underline"
-        href="https://cafe.naver.com/steamindiegame/13461972"
-      >
-        임시공지2
-      </a>
-=======
-
->>>>>>> serverV.2
       <Image src={wowHardcoreLogo} alt="" />
       <ProfileArea />
     </main>
